fd4bccc4ac83b4117dec3704890f4657c9ff776d
SydNichols <sydneynichols.m@gmail.com>
2024-07-13 00:40:19 -0400

    First draft of the unit one project
    This is the project before testing and debugging.

--- js/script.js
+++ js/script.js
@@ -3,31 +3,92 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+/***
+ * `quotes` array
+ ***/
 
-/*** 
- * `quotes` array 
-***/
+//these quotes are from some of my favorite TV shows. Some will be silly, some will be inpirational, all make me laugh! The quotes array will be directly called in the printQuotes function to be passed into the getRandomQuote function.
 
-
+let quotes = [
+  {
+    quote: 'There are no bad ideas... only great ideas that go horribly wrong',
+    source: 'Jack Donaghy (Alec Baldwin)',
+    citation: "30 Rock; Let's Stay Together",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take. - Wayne Gretzky",
+    source: 'Michael Scott (Steve Carell)',
+    citation: 'The Office; Michael Scott Paper Company',
+  },
+  {
+    quote: "You can't just give up. Is that what a dinosaur would do?",
+    source: 'Joey Tribbiani (Matt LeBlanc)',
+  },
+  {
+    quote: 'Don’t start chasing applause and acclaim. That way lies madness.',
+    source: 'Ron Swanson (Nick Offerman)',
+    year: '2012',
+  },
+  {
+    quote: "If you love something, set it free. Unless it's a tiger.",
+    source: 'Phil Dunphy (Ty Burrell)',
+    citation: 'Modern Family; See You Next Fall',
+  },
+]
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
+/*
+ getRandomQuote takes in an array argument. 
+the function will first choose a random number that is between 1 and the length of the array. 
+It will then return an object at the randomNumber index of the array 
+ @param {arr} array - an array of quote object.
+ */
 
+function getRandomQuote(arr) {
+  let randomNumber = Math.floor(Math.random() * arr.length)
 
+  return arr[randomNumber]
+}
 
 /***
  * `printQuote` function
-***/
+ ***/
 
+//printQuote will set a variable to store the getRandomQuote function and directly pass in the quotes array. the html will store the string literal that will be inserted into the html
 
+printQuote = () => {
+  let randomQuote = getRandomQuote(quotes)
+
+  let html = `<p class="quote">${randomQuote.quote}</p>
+                <p class="source">"${randomQuote.source}"
+                `
+  if (randomQuote.citation && randomQuote.year) {
+    html += `<span class="citation">${randomQuote.citation}</span>
+              <span class="year">${randomQuote.year}</span>
+              </p> `
+  } else if (randomQuote.citation) {
+    html += `  <span class="citation">${randomQuote.citation}</span>
+              </p> `
+  } else if (randomQuote.year) {
+    html += `<span class="year">${randomQuote.year}</span>
+              </p> `
+  } else {
+    html += ` </p> `
+  }
+  console.log(randomQuote.quote)
+  console.log(randomQuote.source)
+  console.log(html)
+
+  document.getElementById('quote-box').innerHTML = html
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document
+  .getElementById('load-quote')
+  .addEventListener('click', printQuote, false)
