097e8627591f3ccd76b1f115dd99936a9affe0d2
SydNichols <sydneynichols.m@gmail.com>
2024-07-16 11:52:15 -0400

    Including Random Color Generator
    This commit includes a function that changes the background/text color every time the button is pushed

--- js/script.js
+++ js/script.js
@@ -14,27 +14,60 @@ let quotes = [
     quote: 'There are no bad ideas... only great ideas that go horribly wrong',
     source: 'Jack Donaghy (Alec Baldwin)',
     citation: "30 Rock; Let's Stay Together",
+    tags: ['humor', 'business']
   },
   {
     quote: "You miss 100% of the shots you don't take. - Wayne Gretzky",
     source: 'Michael Scott (Steve Carell)',
     citation: 'The Office; Michael Scott Paper Company',
+    tags: ['humor', 'motivation']
   },
   {
     quote: "You can't just give up. Is that what a dinosaur would do?",
     source: 'Joey Tribbiani (Matt LeBlanc)',
+    tags: ['humor', 'motivation']
   },
   {
-    quote: 'Don’t start chasing applause and acclaim. That way lies madness.',
+    quote: "Don't start chasing applause and acclaim. That way lies madness.",
     source: 'Ron Swanson (Nick Offerman)',
     year: '2012',
+    tags: ['wisdom']
   },
   {
     quote: "If you love something, set it free. Unless it's a tiger.",
     source: 'Phil Dunphy (Ty Burrell)',
     citation: 'Modern Family; See You Next Fall',
-  },
+    tags: ['humor', 'love']
+  }
 ]
+
+/***
+ * `bgColorPalette` array
+ * color palettes chosen from 'https://palettemaker.com/app'
+ ***/
+
+let colorPalette = [
+  { 
+    backgroundColor: '#bd9e84', 
+    textColor: '#c5dfdf' 
+  },
+  { 
+    backgroundColor: '#a71666', 
+    textColor: '#d31638' 
+  },
+  { 
+    backgroundColor: '#d31638', 
+    textColor: '#bd9e84' 
+  },
+  {
+    backgroundColor: '#c5dfdf', 
+    textColor: '#a71666' 
+  },
+  { 
+    backgroundColor: '#e68815', 
+    textColor: '#d31638' 
+  }
+];
 
 /***
  * `getRandomQuote` function
@@ -46,11 +79,23 @@ It will then return an object at the randomNumber index of the array
  @param {arr} array - an array of quote object.
  */
 
-function getRandomQuote(arr) {
+getRandomQuote = (arr) => {
   let randomNumber = Math.floor(Math.random() * arr.length)
 
   return arr[randomNumber]
 }
+
+/***
+ * `getRandomColor` function
+ * this function will choose a random color palette object for the background and text from the colorPalette array
+ */
+getRandomColor = () => {
+  let colorNumber = Math.floor(Math.random() * colorPalette.length);
+  return colorPalette[colorNumber];
+
+}
+
+
 
 /***
  * `printQuote` function
@@ -62,26 +107,25 @@ printQuote = () => {
   let randomQuote = getRandomQuote(quotes)
 
   let html = `<p class="quote">${randomQuote.quote}</p>
-                <p class="source">"${randomQuote.source}"
-                `
-  if (randomQuote.citation && randomQuote.year) {
-    html += `<span class="citation">${randomQuote.citation}</span>
-              <span class="year">${randomQuote.year}</span>
-              </p> `
-  } else if (randomQuote.citation) {
-    html += `  <span class="citation">${randomQuote.citation}</span>
-              </p> `
-  } else if (randomQuote.year) {
-    html += `<span class="year">${randomQuote.year}</span>
-              </p> `
-  } else {
-    html += ` </p> `
+                <p class="source">${randomQuote.source}`
+  if (randomQuote.citation) {
+    html += `<span class="citation">${randomQuote.citation}</span>`
   }
+  if (randomQuote.year) {
+    html += `<span class="year">${randomQuote.year}</span>`
+  }  
+  html += '</p>'
+
+  let newColors = getRandomColor();
+  
+  document.body.style.backgroundColor = newColors.backgroundColor;
+  document.getElementById('quote-box').style.color = newColors.textColor;
+
+  document.getElementById('quote-box').innerHTML = html
+
   console.log(randomQuote.quote)
   console.log(randomQuote.source)
   console.log(html)
-
-  document.getElementById('quote-box').innerHTML = html
 }
 
 /***
